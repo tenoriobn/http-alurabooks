4990bfc1e136e76bf90b47926e44e0557b1a0ee1
Bruno Tenório <tenoriobn@gmail.com>
2023-11-11 10:12:01 -0300

    Configurando o formato dos dados

--- api-alurabooks-main/server.js
+++ api-alurabooks-main/server.js
@@ -357,6 +357,18 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, (req, res, next) => {
   }
 })
 
+server.get('/public/docs', (req, res) => {
+  const meuHtml = `
+    <h1>Documentação da API</h1>
+    <ul>
+      <li>GET /livros</li>
+      <li>POST /livros</li>
+      <li>GET /categorias</li>
+    </ul>
+  `
+  res.status(200).contentType("text/html").send(meuHtml)
+})
+
 server.use(router)
 
 https.createServer({
